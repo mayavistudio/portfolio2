e747f2c1c1abbc64febb6d68141806a55b5e80a0
siddhantchaudhary09 <siddhantchaudhary096@gmail.com>
2024-12-06 16:16:46 +0530

    build-fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 const svgToDataUri = require("mini-svg-data-uri");
 
-const colors = require("tailwindcss/colors");
 const {
   default: flattenColorPalette,
 } = require("tailwindcss/lib/util/flattenColorPalette");
